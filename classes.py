d8ef324d04c40a98eb74aa496dac98ae46492258
zSnails <erizojuan33@gmail.com>
2022-04-02 14:03:25 -0600

    Update Career, Course, AccountType and Account classes to use pydantic BaseModel class

--- classes.py
+++ classes.py
@@ -1,33 +1,37 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 from enum import Enum, auto
 from datetime import date, time
 from typing import List, Tuple
 
 
-class Career(dataclass):
+class Career(BaseModel):
+    """Career class to be used by relationships"""
+
+    id: int
     name: str
 
-
-class Course(dataclass):
+class Course(BaseModel):
     """Course class to be used by each account"""
 
+    id: int
     name: str
     credit: int
     course_hours: int
     start_date: date
     end_date: date
-    schedule: List[Tuple(date, time, time)]
-    belongs_to: List[Career]
+    schedule: List[Tuple[date, time, time]] = []
+    belongs_to: List[str] = []
 
-class AccountType(Enum):
+class AccountType(str, Enum):
     """AccountType enum to be used by accounts"""
 
     STUDENT = auto()
     ADMIN = auto()
 
-class Account(dataclass):
+class Account(BaseModel):
     """Account class to be used by students"""
-
+    
+    id: int
     name: str
-    courses: List[Course]
+    courses: List[Course] = []
     _type: AccountType
