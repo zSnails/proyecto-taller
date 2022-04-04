b6e4a109742a94db5b10162a25b6c318f61405e7
zSnails <erizojuan33@gmail.com>
2022-04-04 02:00:40 -0600

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from enum import Enum, auto
+from enum import IntEnum, auto
 from datetime import date, time
 from typing import List, Tuple
 
@@ -18,19 +18,23 @@ class Course(BaseModel):
     course_hours: int
     start_date: date
     end_date: date
+    # TODO: change the type for each schedule day to a binary number contaning
+    # each of the 7 days of the week, it should be of type int, and the other
+    # two values are fine
     schedule: List[Tuple[date, time, time]] = []
     belongs_to: List[int] = []
 
-class AccountRole(Enum):
+class AccountRole(IntEnum):
     """AccountRole enum to be used by accounts"""
 
-    ADMIN = auto()
     STUDENT = auto()
+    ADMIN = auto()
 
 class Account(BaseModel):
     """Account class to be used by students"""
     
     id: int
     name: str
-    courses: List[Course] = []
+    courses: List[int] = []
+    career: int
     role: AccountRole
