aa455b0020f543a5547087f3d07d9c00ea2f7a9d
zSnails <erizojuan33@gmail.com>
2022-04-04 02:01:10 -0600

    Add default properties to Command 'interface' class

--- command.py
+++ command.py
@@ -1,5 +1,6 @@
 from typing import List
 from enum import Enum, auto
+from classes import AccountRole
 
 class CommandCode(Enum):
     """CommandCode enum for internal command return value handling"""
@@ -8,10 +9,18 @@ class CommandCode(Enum):
     SUCCESS = auto()
     CONTINUE = auto()
     NOT_FOUND = auto()
+    FORBIDDEN = auto()
 
 class Command:
     """Command interface for internal command handling"""
-
+    
     name: str
     aliases: List[str]
+    required_role: AccountRole
+
+    def __init__(self):
+        self.name = "default"
+        self.aliases = []
+        self.required_role = AccountRole.STUDENT
+
     def run(self) -> CommandCode: raise NotImplementedError
