5633e0dde2ff3789d0d64c8d7d2a22920d8c18ed
Akashi <jjlpower105@gmail.com>
2022-04-21 16:11:32 -0600

    Create register career command

--- commands/register_career_cmd.py
+++ commands/register_career_cmd.py
@@ -1,5 +1,4 @@
 from command import Command, CommandCode
-from program import Program
 from models import Career, AccountRole
 class RegisterCareerCommand(Command):
     def __init__(self):
@@ -8,16 +7,16 @@ class RegisterCareerCommand(Command):
         self.aliases = ["rc"]
         self.description = "Starts the career registration daemon"
         self.required_role = AccountRole.ADMIN
-
+        
     def run(self, ctx) -> CommandCode:
         print("====== Registering a new course =======")
-
-        name = input("Enter career name> ")
+        
+        name = input("Enter career name>")
         # if name in ctx.manager.courses:
-        #     print("Ese curso ya existe")
-        #     return CommandCode.ERROR
-        ctx.manager.register_career(Career(id = len(ctx.manager.careers) + 1, name = name))
+        #   print("Ese curso ya existe")
+        #   return CommandCode.Error
+        ctx.manager.register_career(Career(id = len(ctx.manager.careers) +1, name = name))
         return CommandCode.SUCCESS
-
+    
 def setup(program):
     program.load_command(RegisterCareerCommand())
